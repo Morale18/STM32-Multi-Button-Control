7c3b8baf4bea0b46cd9ca93132d1f2cfeb3f77cc
Justinmorales <justinmorales1804@gmail.com>
2025-08-12 11:05:57 -0700

    Update systick.h

--- Inc/systick.h
+++ Inc/systick.h
@@ -1,13 +1,13 @@
-/*
- * systick.h
- *
- *  Created on: Aug 12, 2025
- *      Author: JUSTIN
- */
 
 #ifndef SYSTICK_H_
 #define SYSTICK_H_
+#include <stdint.h>
+
+void SysTick_Init(void);
+void delay(uint32_t ms);
+uint32_t getTicks(void);
 
 
 
 #endif /* SYSTICK_H_ */
+
