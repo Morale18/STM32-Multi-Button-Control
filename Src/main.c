6ae652b3f1999a291b2c215bccf5730d7a1b1252
Justinmorales <justinmorales1804@gmail.com>
2025-08-12 11:07:39 -0700

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -1,8 +1,24 @@
-/*
- * main.c
- *
- *  Created on: Aug 12, 2025
- *      Author: JUSTIN
- */
+#include "gpio.h"
+#include "exti.h"
+#include "systick.h"
+#include "SysClock.h"
+
+volatile int32_t delay_time = 500;
+volatile uint8_t activeLed = 1;
+
+int main(void)
+{
+    init_GPIOs();
+    System_Clock_Init();
+    SysTick_Init();
+    init_EXTI();
+
+    while (1) {
+        delay(delay_time);
+        toggle_led();
+    }
+}
+
+
 
 
